c43877f6ddbb65fa7ec8aea816dad309451c2c73
Duncan <duncanstapleton04@gmail.com>
2025-01-12 11:31:38 -0700

    App.jsx framework and planning

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,107 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useSelector } from "react-redux";
+import { Footer, Navbar } from "./components";
+import { 
+  Outlet, 
+  Navigate, 
+  Route, 
+  Routes, 
+  useLocation 
+} from "react-router-dom";
+import {
+  About,
+  AuthPage,
+  Companies,
+  CompanyProfile,
+  FindJobs,
+  JobDetail,
+  UploadJob,
+  UserProfile,
+} from "./pages";
 
-function App() {
-  const [count, setCount] = useState(0)
+/** Creates a function that checks user authentication *
+ * if the user is authenticated, sends to Outlet       *
+ * if not, sends to Authentication page                */
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Layout() {
+  const { user } = useSelector((state) => state.user);
+  const location = useLocation();
+
+  return user?.token ? (
+    <Outlet />
+  ) : (
+    <Navigate to='/user-auth' state={{ from: location }} replace />
+  );
 }
 
-export default App
+function App() {
+  const { user } = useSelector((state) => state.user);
+  return (
+    <main className='bg-[#f7fdfd]'>
+      <Navbar />
+      <Routes>
+        <Route 
+          element={<Layout />}
+        >
+          <Route
+            path='/'
+            element={
+              <Navigate to='/find-jobs' replace={true} />
+            }
+          />
+
+          <Route 
+            path='/find-jobs' 
+            element={<FindJobs />} 
+          />
+
+          <Route 
+            path='/companies' 
+            element={<Companies/>} 
+          />
+
+          <Route
+            path={
+              user?.user?.accountType === "seeker"
+                ? "/user-profile"
+                : "/user-profile/:id"
+            }
+            element={<UserProfile />}
+          />
+
+          <Route 
+            path={"/company-profile"} 
+            element={<CompanyProfile />} 
+          />
+
+          <Route 
+            path={"/company-profile/:id"} 
+            element={<CompanyProfile />} 
+          />
+
+          <Route 
+            path={"/upload-job"} 
+            element={<UploadJob />} 
+          />
+
+          <Route 
+            path={"/job-detail/:id"} 
+            element={<JobDetail />}
+          />
+        </Route>
+        <Route 
+          path='/about-us' 
+          element={<About />} 
+        />
+
+        <Route 
+          path='/user-auth' 
+          element={<AuthPage />} 
+        />
+      </Routes>
+    {/** Only shows footer is authenticated */}
+      {user && <Footer />}
+    </main>
+  );
+}
+
+export default App;
