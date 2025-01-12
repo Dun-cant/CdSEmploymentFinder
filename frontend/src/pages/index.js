90cb22c0449c1e4c66ad6810dcc75e068422237f
Duncan <duncanstapleton04@gmail.com>
2025-01-12 12:04:09 -0700

    Added index.js and AuthenticationPage based on blueprint

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,19 @@
+import About from "./About";
+import AuthenticationPage from "./Authentication";
+import Companies from "./Companies";
+import CompanyProfile from "./CompanyProfile";
+import FindJobs from "./FindJobs";
+import JobDetail from "./JobDetail";
+import UploadJob from "./UploadJob";
+import UserProfile from "./UserProfile";
+
+export {
+  About,
+  AuthenticationPage,
+  Companies,
+  CompanyProfile,
+  FindJobs,
+  JobDetail,
+  UploadJob,
+  UserProfile,
+};
